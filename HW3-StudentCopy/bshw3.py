d97545bbeee60caa6bd578354ef2701337d9585e
swgould <swgould@umich.edu>
2016-11-10 16:00:15 -0500

    commit

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -23,14 +23,6 @@ for comment in findstudent:
 	fixed_text = str(comment).replace('student', 'AMAZING student')
 	comment.replace_with(fixed_text)
 
-# word = soup.find_all('p')
-# for occurance in word: 
-# 	element = occurance.text
-# 	paragraph = re.findall('\\bstudent\\b', element)
-# 	print (paragraph)
-# 	element = re.sub('\\bstudent\\b', 'AMAZING student', element)
-# 	print (element)
-
 
 for var in soup.find_all(class_='field-items'):
 	for link in var.findAll('img'):
@@ -39,6 +31,11 @@ html_string = str(soup)
 
 for var2 in soup.find_all(class_='logo'):
 	for link in var2.findAll('img'):
+		link['src'] = 'logo.png'
+html_string = str(soup)
+
+for var3 in soup.find_all(class_='footer-logo'):
+	for link in var3.findAll('img'):
 		link['src'] = 'logo.png'
 html_string = str(soup)
 
